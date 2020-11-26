547000dd02e4ef4c97a1acece224640b8aaaab6b
xaur <24484727+xaur@users.noreply.github.com>
2020-11-26 15:33:27 +0000

    simple state machine for connect button

--- src.js
+++ src.js
@@ -1,5 +1,43 @@
 "use strict";
 
+var connectBtn = document.getElementById("connect-btn");
+var websocket;
+
 function connect() {
-  console.log("clicked");
+  console.log("connecting");
+  updateConnectBtn("Cancel connect", disconnect);
+
+  websocket = new WebSocket("ws://echo.websocket.org/");
+
+  console.log("created websocket");
+
+  websocket.onerror = function(evt) {
+    console.log("onerror");
+    updateConnectBtn("Connect", connect);
+  };
+
+  websocket.onclose = function(evt) {
+    console.log("onclose");
+    updateConnectBtn("Connect", connect);
+  };
+
+  websocket.onopen = function(evt) {
+    console.log("onopen");
+    updateConnectBtn("Disconnect", disconnect);
+  };
+
+  websocket.onmessage = function(evt) {
+    console.log("onmessage");
+  };
+}
+
+function updateConnectBtn(label, onclick) {
+  connectBtn.value = label;
+  connectBtn.onclick = onclick;
+}
+
+function disconnect() {
+  websocket.close();
+  websocket = null;
+  updateConnectBtn("Connect", connect);
 }
