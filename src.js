8ce172618ae7beda1d998b4a35b2f3da8352684f
xaur <24484727+xaur@users.noreply.github.com>
2020-11-29 03:46:38 +0000

    update markets table from websocket
    turns out it eats unreasonable amounts of traffic

--- src.js
+++ src.js
@@ -2,9 +2,11 @@
 
 console.log("script eval start");
 
+// UI access
 const connectBtn = document.getElementById("connect-btn");
 const fetchMarketsBtn = document.getElementById("fetch-markets-btn");
 const marketsTable = document.getElementById("markets-table");
+const marketIdToPriceCell = {};
 
 // https://docs.poloniex.com/
 const tickerUrl = "https://poloniex.com/public?command=returnTicker";
@@ -17,6 +19,11 @@ const ws = {
   sock: undefined,
   queue: [],
 };
+
+// stats
+let statsHeartbeats = 0;
+let statsTickerPriceChanges = 0;
+let statsTickerPriceUnchanged = 0;
 
 function initUi() {
   fetchMarketsBtn.disabled = false;
@@ -92,13 +99,21 @@ function onDisconnected(evt) {
 }
 
 function onMessage(evt) {
-  console.log("received: " + evt.data);
   const data = JSON.parse(evt.data);
-  const channel = data[0];
-  const arg2 = data[1];
-  if (arg2 === 1) {
-    console.log("server acked subscription to channel " + channel);
-    return;
+  const [channel, seq] = data;
+  if (channel === 1010) {
+    statsHeartbeats += 1;
+    if (statsHeartbeats % 10 === 0) { 
+      console.log("heartbeats: " + statsHeartbeats);
+    }
+  } else if (channel === 1002) {
+    if (seq === 1) {
+      console.log("ticker subscription server ack");
+      return;
+    }
+    updateMarkets(data);
+  } else {
+    console.log("WARN got data we didn't subscribe for: " + data);
   }
 }
 
@@ -122,27 +137,59 @@ function initMarkets(json) {
   return markets;
 }
 
+function updateMarkets(updates) {
+  // updates look like: [ <chan id>, null,
+  // [ <pair id>, "<last trade price>", "<lowest ask>", "<highest bid>",
+  //   "<percent change in last 24 h>", "<base currency volume in last 24 h>",
+  //   "<quote currency volume in last 24 h>", <is frozen>,
+  //   "<highest trade price in last 24 h>", "<lowest trade price in last 24 h>"
+  // ], ... ]
+  for (let i = 2; i < updates.length; i++) {
+    const [mid, lastPrice] = updates[i];
+    const market = markets[mid];
+    const prevPrice = market.last;
+    if (prevPrice === lastPrice) {
+      statsTickerPriceUnchanged += 1;
+      if (statsTickerPriceUnchanged % 200 === 0) {
+        console.log("ticker price unchanged: " + statsTickerPriceUnchanged);
+      }
+    } else {
+      statsTickerPriceChanges += 1;
+      if (statsTickerPriceChanges % 10 === 0) {
+        console.log("ticker price changes: " + statsTickerPriceChanges);
+      }
+      market.last = lastPrice;
+      marketIdToPriceCell[mid].firstChild.nodeValue = lastPrice;
+      console.log(market.label + " " + prevPrice + " to " + lastPrice);
+    }
+  }
+  if (updates.length > 2 + 1) {
+    console.log("got more than 1 ticker update: " + (updates.length - 2));
+  }
+}
+
 function compareByLabel(a, b) {
   if (a.label < b.label) { return -1; }
   if (a.label > b.label) { return  1; }
   return 0;
 }
 
-function populateMarketsTable(markets) {
-  console.log("markets table populating");
+function createMarketsTable(markets) {
+  console.log("markets table creating");
   marketsTable.innerHTML = "";
   const marketsArr = Object.keys(markets).map((id) => markets[id]);
   marketsArr.sort(compareByLabel);
   marketsArr.forEach((market) => {
     const row = marketsTable.insertRow();
     row.insertCell().appendChild(document.createTextNode(market.label));
-    row.insertCell().appendChild(document.createTextNode(market.last));
+    const td2 = row.insertCell()
+    td2.appendChild(document.createTextNode(market.last));
+    marketIdToPriceCell[market.id] = td2;
   });
-  console.log("markets table populated");
+  console.log("markets table created");
 }
 
 function asyncFetchMarkets() {
-  // TODO: create or update existing markets
   const url = tickerUrl;
   console.log("ticker fetch initiating " + url);
   abortController = new AbortController();
@@ -161,7 +208,7 @@ function asyncFetchMarkets() {
         throw new Error("Poloniex API error: " + json.error);
       }
       markets = initMarkets(json);
-      populateMarketsTable(markets);
+      createMarketsTable(markets);
       return markets;
     })
     .catch(function(e) {
